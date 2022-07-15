b949c32c92b9b8d141e005caa0df0bf2ab9dff7f
Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
2022-07-15 16:26:49 -0400

    fix e2e test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,8 +5,24 @@ export class IgoPage {
     return browser.get('/');
   }
 
-  getNavigatorDiv() {
+  getMenuDiv() {
+    return element(by.css('app-menu'));
+  }
+
+  getHeaderDiv() {
+    return element(by.css('app-header'));
+  }
+
+  getFooterDiv() {
+    return element(by.css('app-footer'));
+  }
+
+  getAppPortalDiv() {
     return element(by.css('app-portal'));
+  }
+
+  getSearchBarInput() {
+    return element(by.css('app-portal igo-search-bar input'));
   }
 
 }
