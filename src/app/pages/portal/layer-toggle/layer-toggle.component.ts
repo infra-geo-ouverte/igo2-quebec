3be5d0c085f0dfd4da53b1dec8867c1c7e8a38ca
mneige <90412486+mneige@users.noreply.github.com>
2022-09-20 16:42:24 -0400

    wip visibilityfromlayertogglebutton

--- src/app/pages/portal/layer-toggle/layer-toggle.component.ts
+++ src/app/pages/portal/layer-toggle/layer-toggle.component.ts
@@ -3,7 +3,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Params } from '@angular/router';
 import { LanguageService } from '@igo2/core';
-import { DataSourceService, IgoMap, Layer, LayerService, OgcFilterableDataSourceOptions, WFSDataSourceOptions } from '@igo2/geo';
+import { DataSourceService, IgoMap, Layer, LayerService, MetadataLayerOptions, MetadataOptions, OgcFilterableDataSourceOptions, WFSDataSourceOptions } from '@igo2/geo';
 import { Subscription } from 'rxjs';
 
 
@@ -47,31 +47,42 @@ export class LayerToggleComponent { // implements OnInit
   ) {
     this.layerService
     .createAsyncLayer({
-      title: 'lieu habité',
-      visible: false,
+      title: 'Régions administratives',
+      visible: true,
       sourceOptions: {
         type: 'wms',
-        url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
+        url: 'https://geoegl.msp.gouv.qc.ca/apis/wss/amenagement.fcgi',
         optionsFromCapabilities: true,
         params: {
-          LAYERS: 'lieuhabite',
+          LAYERS: 'wms_mern_reg_admin',
           VERSION: '1.3.0'
         }
       }
     })
     .subscribe(l => this.map.addLayer(l));
   }
-/*
-  private cancelOngoingAddLayer() {
-    this.addedLayers$$.forEach((sub: Subscription) => sub.unsubscribe());
-    this.addedLayers$$ = [];
-  }
 
-  private visibilityFromLayerToggleButton(
-    params: Params,
-    currentLayerid: string
-  ) {
-    let visible = true;
-  }*/
+  public visibilityFromLayerToggleButton(layers: Layer[]): Layer[] {
+
+    const keepLayerIds = layers.map((layer: Layer) => layer.id);
+
+    layers.forEach((layer: Layer) => {
+      const layerOptions = (layer.options as MetadataLayerOptions) || {};
+      const dataSourceOptions = layer.dataSource.options || {};
+      const metadata = layerOptions.metadata || ({} as MetadataOptions);
+      const keywords = metadata.keywordList || [];
+      const layerKeywords = keywords.map((kw: string) => {
+        return kw.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      });
+
+      //if (layer.title === 'Régions administratives') { //if (value="median")*/
+        layer.visible === false;
+      //}
+    });
+
+    return layers.filter(
+      (layer: Layer) => keepLayerIds.indexOf(layer.id) !== -1
+    );
+  }
 
 }
