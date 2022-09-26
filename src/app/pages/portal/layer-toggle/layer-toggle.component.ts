ecd6b2a77fe3e1370ac238dbb6c79aa4cf69bed4
mneige <90412486+mneige@users.noreply.github.com>
2022-09-26 08:08:03 -0400

    lint and comments cleanup

--- src/app/pages/portal/layer-toggle/layer-toggle.component.ts
+++ src/app/pages/portal/layer-toggle/layer-toggle.component.ts
@@ -1,20 +1,17 @@
-// C:\PROJETS\igo2-lib\packages\geo\src\lib\layer\layer-list\layer-list.component.ts
-import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Params } from '@angular/router';
+import { AfterViewInit, Component, Input, OnDestroy } from '@angular/core';
 import { LanguageService } from '@igo2/core';
-import { DataSourceService, IgoMap, Layer, LayerService, MetadataLayerOptions, MetadataOptions } from '@igo2/geo';
+import { IgoMap, Layer } from '@igo2/geo';
 import { Subscription } from 'rxjs';
-
 
 @Component({
   selector: 'app-layer-toggle',
   templateUrl: './layer-toggle.component.html',
   styleUrls: ['./layer-toggle.component.scss']
 })
-export class LayerToggleComponent implements AfterViewInit, OnDestroy { // OnInit
+export class LayerToggleComponent implements AfterViewInit, OnDestroy {
   @Input() map: IgoMap;
-
- /* @Input()
+/*
+ @Input()
    get toggleLayer(): Layer[] {
     return this._toggleLayers;
   }
@@ -66,41 +63,8 @@ export class LayerToggleComponent implements AfterViewInit, OnDestroy { // OnIni
     }
   }
 
-/*
-  print() {
-    console.log('_toggleLayersLayer:' + this._toggleLayers);
-    for (toggleLayer of this._toggleLayers) {
-
-    }
-  }*/
-
   ngOnDestroy() {
     this.layers$$.unsubscribe();
   }
-/*
-  visibilityFromLayerToggleButton(toggleLayer:Layer) {
-    for (toggleLayer of this._toggleLayers) {
-      if (toggleLayer.title === ('Régions administratives')) {
-        return toggleLayer.visible === true;
-      }
-    }
-  }*/
 
-  //DRAFTS
-
-  /*public visibilityFromLayerToggleButton(layers: Layer[]) {
-    //const keepLayerIds = layers.map((layer: Layer) => layer.id);
-
-    layers.forEach ((layer: Layer) => {
-      //const layerOptions = (layer.options as MetadataLayerOptions) || {};
-      //const dataSourceOptions = layer.dataSource.options || {};
-      //const metadata = layerOptions.metadata || ({} as MetadataOptions);
-        if (layer.title === 'Régions administratives') { //if (value="median")
-          return layer.visible === true;
-      }
-    });
-
-  }*/
-
-  //this._toggleLayers = activeLayers.filter(l => !l.baseLayer);
 }
