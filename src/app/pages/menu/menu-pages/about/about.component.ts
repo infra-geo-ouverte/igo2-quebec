da8be59e48d273828861e1ddc167553edcaae9fe
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-08-02 15:33:35 -0400

    update style

--- src/app/pages/menu/menu-pages/about/about.component.ts
+++ src/app/pages/menu/menu-pages/about/about.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
+  styles: [
+    ':host {overflow: auto}'
+  ]
 })
 export class AboutComponent {
 
