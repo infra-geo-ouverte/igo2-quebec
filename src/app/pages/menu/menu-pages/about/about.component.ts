99b058149783c3e4f3249fbcfb4fa2161272e6b2
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-08-11 13:06:43 -0400

    fix qcca theme

--- src/app/pages/menu/menu-pages/about/about.component.ts
+++ src/app/pages/menu/menu-pages/about/about.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styles: [
-    ':host {overflow: auto}'
-  ]
 })
 export class AboutComponent {
 
