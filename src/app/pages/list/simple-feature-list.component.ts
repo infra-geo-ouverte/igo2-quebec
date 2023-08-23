a174c855117d8bd72e042666d0e983007ec7b680
matt-litwiller <matthew.litwiller@msp.gouv.qc.ca>
2023-08-23 10:21:37 -0400

    refactor(simpleFeatureList): reformatted regex

--- src/app/pages/list/simple-feature-list.component.ts
+++ src/app/pages/list/simple-feature-list.component.ts
@@ -314,7 +314,6 @@ export class SimpleFeatureListComponent implements OnInit, OnChanges, OnDestroy 
     });
     // remove the square brackets surrounding the attributes
     personalizedAttribute = personalizedAttribute.replace(/[\[\]]/g, '');
-    personalizedAttribute = personalizedAttribute.replace(/^([^A-zÀ-ÿ0-9])*|([^A-zÀ-ÿ0-9])*$/g, '');
     return personalizedAttribute;
   }
 
@@ -339,7 +338,7 @@ export class SimpleFeatureListComponent implements OnInit, OnChanges, OnDestroy 
    */
   isEmail(attribute: any): any {
     let possibleEmail: string = '' + attribute;
-    const match: Array<string> = possibleEmail.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+    const match: Array<string> = possibleEmail.match(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/);
     const message = this.languageService.translate.instant('simpleFeatureList.email');
     if (match && this.formatEmail) {
       return `<a href="mailto:${match[0]}">${message}</a>`;
