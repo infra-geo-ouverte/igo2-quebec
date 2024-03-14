b2d08a1a686cb755261498a59391a59c077b2cb9
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2024-03-14 09:58:00 -0400

    feat(panels): create a search-result panel

---
+++ src/app/pages/portal/panels/panels-handler/panels/search-results/search-results-panel.component.ts
@@ -0,0 +1,83 @@
+import { AsyncPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output
+} from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+
+import { IgoMap, Layer, SearchResultsComponent } from '@igo2/geo';
+import { SearchState } from '@igo2/integration';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+import { SearchResultAction } from '../../panels-handler.enum';
+import {
+  onResultFocus,
+  onResultSelect,
+  onResultUnfocus,
+  onSearch
+} from './search-results-panel.utils';
+
+@Component({
+  selector: 'app-search-results-panel',
+  templateUrl: './search-results-panel.component.html',
+  styleUrls: ['./search-results-panel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    MatTooltip,
+    MatIconButton,
+    MatIcon,
+    TranslateModule,
+    SearchResultsComponent,
+    AsyncPipe
+  ]
+})
+export class SearchResultPanelComponent implements OnInit, OnDestroy {
+  public searchResultActions = SearchResultAction;
+
+  @Input() map: IgoMap;
+  @Input() searchState: SearchState;
+
+  @Output() closed = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  onSearchTermChange(term: string) {
+    this.searchState.setSearchTerm(term);
+  }
+
+  onResult(searchResultAction: SearchResultAction, event) {
+    console.log('searchResultAction', searchResultAction, event);
+    switch (searchResultAction) {
+      case SearchResultAction.Focus:
+        onResultFocus();
+        break;
+      case SearchResultAction.Search:
+        onSearch();
+        break;
+      case SearchResultAction.Select:
+        onResultSelect();
+        this.close();
+        break;
+      case SearchResultAction.Unfocus:
+        onResultUnfocus();
+        break;
+    }
+  }
+
+  close() {
+    this.closed.emit(true);
+  }
+}
---
+++ src/app/pages/portal/panels/panels-handler/panels/search-results/search-results-panel.utils.ts
@@ -0,0 +1,12 @@
+export function onResultFocus() {
+  console.log('onResultFocus');
+}
+export function onResultSelect() {
+  console.log('onResultSelect');
+}
+export function onResultUnfocus() {
+  console.log('onResultUnfocus');
+}
+export function onSearch() {
+  console.log('onSearch');
+}
