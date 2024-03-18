62119a7aac4c8295beaa33d2c0bbcd760d868da4
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2024-03-18 16:07:49 -0400

    wip

--- src/app/pages/portal/map-overlay/map-overlay.component.ts
+++ src/app/pages/portal/map-overlay/map-overlay.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 
 import { Context, ContextService } from '@igo2/context';
@@ -14,7 +14,7 @@ import { MapOverlay } from './map-overlay.interface';
   templateUrl: './map-overlay.component.html',
   styleUrls: ['./map-overlay.component.scss'],
   standalone: true,
-  imports: [NgClass, NgStyle]
+  imports: [NgFor, NgClass, NgIf, NgStyle]
 })
 export class MapOverlayComponent implements AfterViewInit, OnDestroy {
   public mapOverlay: MapOverlay[] = [];
@@ -44,14 +44,14 @@ export class MapOverlayComponent implements AfterViewInit, OnDestroy {
   }
 
   private handleContextChange(context: Context) {
-    let mapOverlay = [];
+    let mapOverlay: MapOverlay[] = [];
     if (context !== undefined) {
       this.mapOverlay = [];
 
       if (context['mapOverlay']) {
         mapOverlay = context['mapOverlay'];
-      } else if (this.configService.getConfig('mapOverlay')) {
-        mapOverlay = this.configService.getConfig('mapOverlay');
+      } else {
+        mapOverlay = this.configService.getConfig('mapOverlay', []);
       }
       for (const overlay of mapOverlay) {
         // If no media define use default to desktop, display only if current media is on context definition
